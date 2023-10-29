f4968a26bd43700df644f778a0ac2c1df75068cb
RamyaShetty81 <120573688+RamyaShetty81@users.noreply.github.com>
2023-10-29 19:44:24 +0530

    Update

--- SunbaseData/src/main/java/com/example/SunbaseData/CustomerService.java
+++ SunbaseData/src/main/java/com/example/SunbaseData/CustomerService.java
@@ -1,16 +1,21 @@
 package com.example.SunbaseData;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.SunbaseData.DTOs.CustomerRequest;
+import com.example.SunbaseData.DTOs.CustomerResponse;
 
 @Service
 public interface CustomerService {
 
-    public String addCustomer(Customer customer) throws Exception;
+    public String addCustomer(CustomerRequest customerRequest) throws Exception;
 
     public String deleteCustomer(Integer customerId) throws Exception;
 
-    public String updateCustomer(Customer customer) throws Exception;
+    public String updateCustomer(Integer id, CustomerRequest customerRequest) throws Exception;
 
-    public String getCustomerList() throws Exception;
+    public List<CustomerResponse> getCustomerList() throws Exception;
     
 }
